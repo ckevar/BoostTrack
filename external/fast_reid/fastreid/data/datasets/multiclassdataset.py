924f4a1b678e3ef420d9c2c00e25136f2d8772cf
ckevar <ckevar@gmail.com>
2025-10-01 14:25:32 +0200

    modified:   external/fast_reid/fastreid/data/datasets/multiclassdataset.py

--- external/fast_reid/fastreid/data/datasets/multiclassdataset.py
+++ external/fast_reid/fastreid/data/datasets/multiclassdataset.py
@@ -1,6 +1,45 @@
 from fast_reid.fastreid.data.datasets import DATASET_REGISTRY
 from fast_reid.fastreid.data.datasets.bases import ImageDataset
 import os
+import numpy as np
+
+def build_pid_map(root_dir, txt_files):
+
+    if os.path.isfile("pid_map.npz"):
+        data = np.load("pid_map.npz")
+        pids = data["pids"]
+        labels = data["labels"]
+        return {pid: label for pid, label in zip(pids, labels)}
+
+    all_pids = set()
+    for txt in txt_files:
+        with open(txt) as f:
+            for line in f:
+                _, pid, _ = line.strip().split()
+                all_pids.add(int(pid))
+
+    pid2label = {pid: idx for idx, pid in enumerate(sorted(all_pids))}
+
+    # Store PIDs to avoid recomputation
+    pids = np.array(list(pid2label.keys()), dtype=np.int64)
+    labels = np.array(list(pid2label.values()), dtype=np.int64)
+
+    np.savez("pid_map.npz", pids=pids, labels=labels)
+
+    return pid2label
+
+def process_txt(root, txt_file, pid2label):
+    data = []
+    with open(txt_file) as f:
+        for line in f:
+            path, pid, camid = line.strip().split()
+            pid = int(pid)
+            camid = int(camid)
+            data.append((os.path.join(root, path), 
+                         pid2label[pid], 
+                         camid))
+
+    return data
 
 @DATASET_REGISTRY.register()
 class MulticlassMOT17(ImageDataset):
@@ -9,17 +48,14 @@ class MulticlassMOT17(ImageDataset):
     def __init__(self, root='datasets', **kwargs):
         self.root = os.path.join(root, self.dataset_dir)
         # TODO: Edit this to point where the train.txt file is
-        train = self._load_list('train.txt')
-        query = self._load_list('query.txt')
-        gallery = self._load_list('gallery.txt')
+        train_txt   = os.path.join(self.root, "train.txt")
+        query_txt   = os.path.join(self.root, "query.txt")
+        gallery_txt = os.path.join(self.root, "gallery.txt")
+
+        pid2label = build_pid_map(root, [train_txt, query_txt, gallery_txt])
+
+        train   = process_txt(self.root, train_txt, pid2label)
+        query   = process_txt(self.root, query_txt, pid2label)
+        gallery = process_txt(self.root, gallery_txt, pid2label)
 
         super().__init__(train, query, gallery, **kwargs)
-
-    def _load_list(self, fname):
-        data = []
-        with open(os.path.join(self.root, fname)) as f:
-            for line in f:
-                img_path, pid, camid = line.strip().split()
-                data.append((os.path.join(self.root, img_path), int(pid), int(camid)))
-        return data
-
