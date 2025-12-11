953dce3e2eab2d1b98db5b1fe820fdc3865ec932
ckevar <ckevar@gmail.com>
2025-12-11 14:01:37 +0100

    modified:   external/fast_reid/fastreid/evaluation/evaluator.py

--- external/fast_reid/fastreid/evaluation/evaluator.py
+++ external/fast_reid/fastreid/evaluation/evaluator.py
@@ -113,6 +113,7 @@ def inference_on_dataset(model, data_loader, evaluator, flip_test=False):
     total_compute_time = 0
     with inference_context(model), torch.no_grad():
         for idx, inputs in enumerate(data_loader):
+            print("idx/total", idx, total)
             if idx == num_warmup:
                 start_time = time.perf_counter()
                 total_compute_time = 0
